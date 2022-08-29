c4eba2a133ae752b53773ecf569c36ef9832d5bd
FF <1396933943@qq.com>
2022-08-29 15:23:57 +0800

    feat: 处理打包文件

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# 一、简介
+![https://www.npmjs.com/package/ff-authority](https://img.shields.io/npm/v/ff-authority)
+![https://www.npmjs.com/package/ff-authority](https://img.shields.io/npm/dt/ff-authority)
+![https://github.com/wang12321/ff-authority](https://img.shields.io/github/languages/code-size/wang12321/ff-authority)
+![https://github.com/wang12321/ff-authority](https://img.shields.io/github/issues-raw/wang12321/ff-authority)
+![https://github.com/wang12321/ff-authority](https://img.shields.io/github/license/wang12321/ff-authority)
+### ff-authority 是权限中间件
+# 使用环境(推荐)
+node > 10.x
+
+npm > 6.x
+
+vue 2.x
+
+elementUI 2.15.X (其他版本也可以)
+
+
+# 二、使用方法
+### 安装
+
+```sh
+npm i ff-authority
+```
+
+main.js
+```angular2html
+
+```
+
--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,41 @@ var gulp = require('gulp');
 const uglify = require('gulp-uglify');
 var bump = require('gulp-bump');
 
-gulp.task('js',['bump'], function () {
+gulp.task('js',['bump','directive','sidebar','mixin'], function () {
     return gulp.src('./bin/*.js') // read all
         .pipe(uglify())
         .pipe(gulp.dest('./lib'));
+});
+
+gulp.task('directive', function () {
+    return gulp.src('./bin/directive/*.js') // read all
+        .pipe(uglify())
+        .pipe(gulp.dest('./lib/directive'));
+});
+
+gulp.task('mixin', function () {
+    return gulp.src('./bin/mixin/*.js') // read all
+        .pipe(uglify())
+        .pipe(gulp.dest('./lib/mixin'));
+});
+gulp.task('sidebarViewCss', function () {
+    return gulp.src('./bin/sidebar/*.scss') // read all
+        .pipe(gulp.dest('./lib/sidebar'));
+});
+gulp.task('sidebarView', function () {
+    return gulp.src('./bin/sidebar/*.vue') // read all
+        .pipe(gulp.dest('./lib/sidebar'));
+});
+gulp.task('sidebar',['sidebarView','sidebarViewCss'], function () {
+    return gulp.src('./bin/sidebar/*.js') // read all
+        .pipe(uglify())
+        .pipe(gulp.dest('./lib/sidebar'));
+});
+
+gulp.task('bump',function(){
+    return gulp.src('./package.json')
+        .pipe(bump({type:'patch'}))
+        .pipe(gulp.dest('./'));
 });
 
 gulp.task('bump',function(){
